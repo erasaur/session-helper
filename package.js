f263697332ee6794fc82120fd1a04c7447841b33
erasaur <erasaur@gmail.com>
2014-11-21 20:03:05 -0800

    Reorganize files and add template helper

--- package.js
+++ package.js
@@ -1,16 +1,17 @@
 Package.describe({
   name: 'erasaur:session-helper',
   summary: 'Template Helper for Sessions',
-  version: '1.0.0',
+  version: '0.0.1',
   git: 'https://github.com/erasaur/session-helper'
 });
 
-Package.onUse(function(api) {
+Package.onUse(function (api) {
   api.versionsFrom('1.0');
-  api.addFiles('erasaur:session-helper.js');
+  api.use('templating', 'client');
+  api.addFiles('erasaur:session-helper.js', 'client');
 });
 
-Package.onTest(function(api) {
+Package.onTest(function (api) {
   api.use('tinytest');
   api.use('erasaur:session-helper');
   api.addFiles('erasaur:session-helper-tests.js');
